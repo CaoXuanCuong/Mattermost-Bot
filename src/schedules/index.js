5cd787106b1f372f472b117a5f16f386132e03c2
Cuong CX <cuongcx@bsscommerce.com>
2025-07-30 18:09:15 +0700

    set bot schedule

--- src/schedules/index.js
+++ src/schedules/index.js
@@ -4,44 +4,29 @@ const {
 } = require("../mm_requests/bot_request");
 
 const autoRemindReportTasks = async (Request) => {
-  const reportTasksMessage = `:alarm_clock: Giờ lành đã điểm, anh chị em, sinh hoạt buồng! @cuongcx_762, @dongdx_1222, @cuonghv_1099, @vanntt_1107, @dungntp1_1221.\n
-    Reply vào thread này để báo cáo nha, theo mẫu sau:\n\n
-
-    I. Những task đang làm\n
-    1. Update: Storefront - Popup\n
-    Link: https://pms.bssgroup.vn/default/viewtaskdetail/SABLPR-3\n
-    Tiến độ: 85%\n
-    Vấn đề: Không\n
-    Dự kiến done: Ngày mai\n
-
-    2. CMS - Customers\n
-    Link: https://pms.bssgroup.vn/default/viewtaskdetail/SABLPR-8\n
-    Tiến độ: 90%\n
-    Vấn đề: Không\n\n
-
-    II. Những task dự kiến làm\n
-    Không có`
+  const reportTasksMessage = `:alarm_clock: Giờ lành đã điểm, anh chị em, sinh hoạt buồng! @cuongcx_762, @dongdx_1222, @cuonghv_1099, @vanntt_1107, @dungntp1_1221.\nReply vào thread này để báo cáo nha, theo mẫu sau:\n\nI. Những task đang làm\n1. Update: Storefront - Popup\nLink: https://pms.bssgroup.vn/default/viewtaskdetail/SABLPR-3\nTiến độ: 85%\nVấn đề: Không\nDự kiến done: Ngày mai\n2. CMS - Customers\nLink: https://pms.bssgroup.vn/default/viewtaskdetail/SABLPR-8\nTiến độ: 90%\n Vấn đề: Không\n\nII. Những task dự kiến làm\nKhông có`
   ;
   await Request.send({ message: reportTasksMessage });
 };
 
 const autoRemindSprintMeeting = async (Request) => {
-  const reportTasksMessage = `Buồng mình có lịch gặp mặt sinh hoạt tại lòng 19L lớn lúc 15h45 nha anh chị em @all.\n
-    Đề nghị các anh chị @cuongcx_762 chuẩn bị báo cáo tiến độ chung, @huongpt_694 chuẩn bị báo cáo tình hình business, @dongdx_1222, @cuonghv_1099 sẵn sàng demo và @vanntt_1107 update file test tracking!`;
+  const reportTasksMessage = `:speaking_head_in_silhouette: Buồng mình có lịch gặp mặt sinh hoạt tại lòng 19L lớn lúc 15h50 nha anh chị em @all.\nĐề nghị các anh chị @cuongcx_762 chuẩn bị báo cáo tiến độ chung, @huongpt_694 chuẩn bị báo cáo tình hình business, @vynmk_1189 báo cáo tình hình support khách và @vanntt_1107 update file test tracking!`;
   await Request.send({ message: reportTasksMessage });
 };
 
 const Schedule = {
   init: () => {
-    const daysOfWeek = [1, 2, 3, 4, 5];
+    const reportTasksDays = [2, 4];
+    const sprintMeetingDays = [1];
+
     schedule.scheduleJob(
-      { tz: "Asia/Ho_Chi_Minh", dayOfWeek: daysOfWeek, hour: 18, minute: 3 },
+      { tz: "Asia/Ho_Chi_Minh", dayOfWeek: reportTasksDays, hour: 17, minute: 30 },
       () => {
         autoRemindReportTasks(MMBotRequest);
       },
     );
     schedule.scheduleJob(
-      { tz: "Asia/Ho_Chi_Minh", dayOfWeek: daysOfWeek, hour: 18, minute: 3 },
+      { tz: "Asia/Ho_Chi_Minh", dayOfWeek: sprintMeetingDays, hour: 15, minute: 20 },
       () => {
         autoRemindSprintMeeting(MMBotRequest);
       },
